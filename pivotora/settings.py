17fcd56c01cedad0db7989ed3ebe6da186d52220
mhashirhassan22 <mhashirhassan22@gmail.com>
2021-01-13 01:43:25 +0500

    v

--- pivotora/settings.py
+++ pivotora/settings.py
@@ -132,7 +132,6 @@ PROJECT_ROOT = (os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'media'),
     # Extra lookup directories for collectstatic to find static files
 )
 

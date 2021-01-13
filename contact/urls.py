c73170dcf75957d1b940220d946a9dfbd67f9ac5
mhashirhassan22 <mhashirhassan22@gmail.com>
2021-01-13 21:23:38 +0500

    check

--- contact/urls.py
+++ contact/urls.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 app_name = 'contact'
 urlpatterns = [
-        path('', PivotoraIndex.as_view() , name="index"),
+        path('', views.CalendarView.as_view() , name="index"),
         url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
         url(r'^event/new/$', views.event, name='event_new'),
 	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),  
